505b18adf6729678d9003e69649981de2e216aa0
gumodi35 <gumodi10@gmail.com>
2022-05-14 22:27:32 -0500

    completando el formulario de usuario

---
+++ Web/JS/main.js
@@ -0,0 +1,12 @@
+//validacion del formulario
+const btnSuscribirte = document.getElementById('suscribirte');
+
+const validacion = (e) => {
+    e.preventDefault();
+    //declaramos nuestras variables que son los inputs del form
+    const nombreUsuario = document.getElementById("name");
+    const apellidoUsuario = document.getElementById("apellido");
+    const emailUsuario = document.getElementById("email");
+    const edadUsuario = document.getElementById("edad");
+    const ciudadUsuario = document.getElementById("ciudad")
+}
