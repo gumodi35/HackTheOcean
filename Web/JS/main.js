fed2f56bfcb1800bebe0c2ec630b963b0598eefc
gumodi35 <gumodi10@gmail.com>
2022-05-15 02:01:15 -0500

    corrigiendo la validacion del formularion en JS

--- Web/JS/main.js
+++ Web/JS/main.js
@@ -1,18 +1,17 @@
 //validacion del formulario
+// -> declaramos nuestras variables que son los inputs del form
 const btnSuscribirte = document.getElementById('suscribirte');
 
 const validacion = (e) => {
     e.preventDefault();
-    //declaramos nuestras variables que son los inputs del form
     const nombreUsuario = document.getElementById("nombre");
     const apellidoUsuario = document.getElementById("apellido");
     const emailUsuario = document.getElementById("email");
     const edadUsuario = document.getElementById("edad");
-    const ciudadUsuario = document.getElementById("ciudad")
-    const diaUsuario = document.getElementById("")
-    const playaUsuario = document.getElementById("playa")
+    const ciudadUsuario = document.getElementById("ciudad");
+    const playaUsuario = document.getElementById("playa");
 
-    // hacemos los casos de validacion para los id
+    // -> hacemos los casos de validacion para los id
     if (nombre.value === "") {
         alert("Ingresa, por favor tu nombre de usuario.");
         nombre.focus();
@@ -26,31 +25,35 @@ const validacion = (e) => {
     }
 
     if (email.value === "") {
-        alert("Ingresa, por favor tu email")
+        alert("Ingresa, por favor tu email");
         email.focus();
         return false;
     }
 
     if (edad.value === "") {
-        alert("Ingresa, por favor tu edad")
+        alert("Ingresa, por favor tu edad");
         edad.focus();
         return false;
     }
 
     if (ciudad.value === "") {
-        alert("Ingresa, por favor tu ciudad")
+        alert("Ingresa, por favor tu ciudad");
         ciudad.focus();
         return false;
     }
 
     if (playa.value === "") {
-        alert("Ingresa, por favor tu playa")
+        alert("Ingresa, por favor tu playa");
         ciudad.focus();
         return false;
     }
 
-    return true;
+    //return true;
 
+    // -> El formulario se envia
+    alert("Gracias por participar, tus datos se enviaron");
+    document.validacion.submit();
 }
 
+// -> Agregamos el evento al boton
 btnSuscribirte.addEventListener('click', validacion);
